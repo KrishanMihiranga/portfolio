9f83a0a102d8834b399c689f7509f4c977a9e6ef
KrishanMihiranga <xp3cto95@gmail.com>
2024-07-20 23:23:29 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css';
+import './styles/customStyles.css';
+function App() {
+  return (
+    <>
+      <div className='w-full h-full bg-black justify-center items-center flex'>
+        <div className='flex flex-col items-center justify-center'>
+          <h3 className='text-custom'>Hello World</h3>
+          <h1 className='text-custom'>My Name is Krishan</h1>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
